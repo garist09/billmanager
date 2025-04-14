d7c3ab235f68ab486e5faba890be47fcfea29095
garist09 <garist09@mail.ru>
2025-04-14 23:31:56 +0300

    Initialize the project

---
+++ src/test/java/com/rg/billmanager/BillManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rg.billmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
